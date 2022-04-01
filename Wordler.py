d19768f37d34319b58540b8515af75e8a38bb54e
PPCCCSCS <46571983+PPCCCSCS@users.noreply.github.com>
2022-04-01 11:10:36 -0600

    Add files via upload
    Initial Color Scheme Test

--- Wordler.py
+++ Wordler.py
@@ -7,7 +7,7 @@ def main():
     root = tk.Tk()
     root.title("Wordler-Wordle-Guesser")
 
-    f = open("Wordle.csv",'r')
+    f = open("Wordle.txt",'r')
     word_list = f.read().rsplit(",")
     print(len(word_list))
 
@@ -25,7 +25,7 @@ def main():
                 width=3,
                 height=1,
                 bg="grey",
-                fg="black"
+                fg="white"
                 )
             btn.grid(row=i+1,column=j,columnspan=5,sticky="W")
             row.append(btn)
@@ -40,8 +40,31 @@ def main():
                          )
     btnGuess.grid(row=0,column=0,columnspan=5,pady=2)
 
-    #TEST
-    ButtonArray[0][0].config(text="F")
+    '''#TEST'''
+    ButtonArray[0][0].config(text="S",bg="green")
+    ButtonArray[0][1].config(text="T",bg="orange")
+    ButtonArray[0][2].config(text="A",bg="grey")
+    ButtonArray[0][3].config(text="I",bg="grey")
+    ButtonArray[0][4].config(text="R",bg="grey")
+
+    ButtonArray[1][0].config(text="C",bg="grey")
+    ButtonArray[1][1].config(text="L",bg="grey")
+    ButtonArray[1][2].config(text="O",bg="green")
+    ButtonArray[1][3].config(text="N",bg="orange")
+    ButtonArray[1][4].config(text="E",bg="grey")
+
+    ButtonArray[2][0].config(text="S",bg="green")
+    ButtonArray[2][1].config(text="N",bg="green")
+    ButtonArray[2][2].config(text="O",bg="green")
+    ButtonArray[2][3].config(text="T",bg="orange")
+    ButtonArray[2][4].config(text="S",bg="grey")
+
+    ButtonArray[3][0].config(text="S",bg="green")
+    ButtonArray[3][1].config(text="N",bg="green")
+    ButtonArray[3][2].config(text="O",bg="green")
+    ButtonArray[3][3].config(text="U",bg="green")
+    ButtonArray[3][4].config(text="T",bg="green")
+    '''#TEST'''
     
     #window = Wordler(root)
     root.mainloop()
