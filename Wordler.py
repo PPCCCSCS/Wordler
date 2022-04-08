0229c086f24562350d0993c6f6f6b85e02357b40
PPCCCSCS <46571983+PPCCCSCS@users.noreply.github.com>
2022-04-08 09:56:10 -0600

    Add files via upload
    Fixed a bug in bad_letters maintenance.

--- Wordler.py
+++ Wordler.py
@@ -6,12 +6,10 @@ import random
 
 MAX_ROW = 10
 STATES = ["grey","orange","green"]
-GO_STS = ["black","orange","green"]
 c_row = 0 # Keep track of which row we're currently on
 guesses = list()
 bad_letters = "."
 not_here = ["","","","",""]
-
 
 def check_char_in_pos(word,char,pos):
     if word[pos] == char:
@@ -41,19 +39,10 @@ def check_unique(word):
         return False        
 
 def btn_clicked(i,j,ButtonArray):
-
     bg_next = STATES[(STATES.index(ButtonArray[i][j].cget('bg'))+1)%len(STATES)]
-
-    '''
-    if i < 6:
-        bg_next = STATES[(STATES.index(ButtonArray[i][j].cget('bg'))+1)%len(STATES)]
-    else:
-        bg_next = GO_STS[(GO_STS.index(ButtonArray[i][j].cget('bg'))+1)%len(GO_STS)]
-    '''
     ButtonArray[i][j].config(bg=bg_next)
 
 def wfr(BA,row):
-
     return BA[row][0]['text']+\
            BA[row][1]['text']+\
            BA[row][2]['text']+\
@@ -63,18 +52,13 @@ def wfr(BA,row):
 # For filtering a SINGLE word in dictionary
 # Use with FILTER to build matching dictionary
 def check_guess_result(word,prev,result):
-
     global guesses
     global bad_letters
     global not_here
-
     if word in guesses:
         return False
-
     for i,r in enumerate(result):
         if r == "grey":
-            if prev[i] not in bad_letters:
-                bad_letters += prev[i]
             if word[i] in not_here[i]:
                 return False
             for j,letter in enumerate(word):
@@ -100,9 +84,6 @@ def btn_guessed(BA,lWU,dW):
     global guesses
     global bad_letters
     dctFiltered = dict()
-
-    #print("lWU:",len(lWU),"dW",len(dW),"greys:",bad_letters)
-    
     # Pick the max(dctWeightedUnique,key=dctWeighted.get)
     if c_row == 0:
         guess = max(lWU,key=dW.get)
@@ -110,25 +91,26 @@ def btn_guessed(BA,lWU,dW):
             BA[0][i].config(text=ch)
     # Read the color of the letters for previous guess, filter accordingly
     elif c_row < MAX_ROW:
-
         result = [BA[c_row-1][0]['bg'],
                   BA[c_row-1][1]['bg'],
                   BA[c_row-1][2]['bg'],
                   BA[c_row-1][3]['bg'],
                   BA[c_row-1][4]['bg']]
-
-        #print(result)
-
         prev = wfr(BA,c_row - 1)
-        #print("prev was:", prev)
+        oranges = "."
+        greys   = "."
+        for btn in BA[c_row-1]:
+            if btn['bg'] == "grey":
+                greys += btn['text']
+            elif btn['bg'] == "orange":
+                oranges += btn['text']
+            for i in greys:
+                if i not in oranges:
+                    if i not in bad_letters:
+                        bad_letters += i
         for word in dW:
-
-            #print("previous word was:", prev)
             if check_guess_result(word,prev,result):
                 dctFiltered[word] = dW[word]
-                #print(word)
-
-        #print(len(dctFiltered))
         if len(dctFiltered) > 0:
             guess = max(dctFiltered,key=dW.get)
             guesses.append(guess)
@@ -136,34 +118,20 @@ def btn_guessed(BA,lWU,dW):
                 BA[c_row][i].config(text=ch)
                 if BA[c_row-1][i]['bg'] == "green":
                     BA[c_row][i].config(bg="green")
-        
     else:
         print("gameover")
-
     if c_row < MAX_ROW:
-        '''
-        print("Guess",c_row,"=",
-              BA[c_row][0]['text']+\
-              BA[c_row][1]['text']+\
-              BA[c_row][2]['text']+\
-              BA[c_row][3]['text']+\
-              BA[c_row][4]['text'])
-        '''
         c_row+=1
  
 def main():
     root = tk.Tk()
     root.title("Wordler-Wordle-Guesser")
     tkFont = font.Font(size=24)
-
     f = open("Wordle.txt",'r')
     word_list = f.read().upper().strip().rsplit(",")
-
     dctFrequency = dict()
     lstFrequency = ""
     dctWeighted = dict()
-    
-    
     # Count the occurrences of each letter in the Wordle Word List
     for word in word_list:
         for char in word:
@@ -171,39 +139,23 @@ def main():
                 dctFrequency[char] += 1
             else:
                 dctFrequency[char] = 1
-
     # Create a list of letters sorted most common to least
     for ltr in sorted(dctFrequency,key=dctFrequency.get,reverse=True):
         lstFrequency += ltr
-
-    #print(lstFrequency)
-
     # Create a dictionary with a weighted rarity score for each word
     for word in word_list:
         dctWeighted[word] = 0
         for char in word:
             dctWeighted[word] += dctFrequency[char]
-
     lstWeightedUnique = list(filter(check_unique, dctWeighted))
-
     dctWeightedUnique = dict()
-
     for item in lstWeightedUnique:
         dctWeightedUnique[item] = dctWeighted[item]
-
-    '''#TEST'' '
-    print(dctWeightedUnique)
-    print(max(dctWeightedUnique,key=dctWeighted.get))
-    print(min(dctWeightedUnique,key=dctWeighted.get))
-    '''#TEST'''
-
     # Build the game window
     winMain = tk.Frame(root)
     winMain.grid(row=0,column=0)
-
     # Each button will correspond to a single letter
     ButtonArray = []
-
     # Build the Letter Button Array, Clickable for Context
     for i in range(MAX_ROW):
         row = []
@@ -219,14 +171,9 @@ def main():
                 command=lambda i=i,j=j,ButtonArray=ButtonArray: \
                 btn_clicked(i,j,ButtonArray)
                 )
-            '''
-            if i > 5:
-                btn['bg'] = "black"
-            '''
             btn.grid(row=i+1,column=j,columnspan=5,sticky="W")
             row.append(btn)
         ButtonArray.append(row)
-
     # create the guess button here
     btnGuess = tk.Button(winMain,
                          text="GUESS WORD",
@@ -244,7 +191,6 @@ def main():
                          )
     # place the guess button at the top of the game window
     btnGuess.grid(row=0,column=0,columnspan=5,pady=2)
-  
     #window = Wordler(root)
     root.mainloop()
 
